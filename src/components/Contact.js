0afba124292c655b9444016c4dae1679cb2dae27
Rahul Nahar <rahul.nahar@cimpress.com>
2022-02-09 18:42:48 +0530

    changes to contact us and pagination

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,19 +4,29 @@ import styled from 'styled-components'
 const Contact = () => {
   return <Wrapper>
     <div className="section-center">
-      <h3>Join our newsletter and get 20% off</h3>
-      <div className="content">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati culpa quod fuga eveniet error. Necessitatibus ducimus vero repellat sint ad?
+      <div className='title'>
+        <h2>contact us</h2>
+        <div className='underline'></div>
+      </div>
+      <div className="content service">
+        <p>116, MAHAVEER SADAN
+          KABUTAR KHANA CIRCLE, Bhuleshwar Road,
+          NEAR RAM MANDIR,
+          Mumbai, Maharashtra 400002
         </p>
-        <form className="contact-form"
-          action="https://formspree.io/f/xrgrrbnk"
-          method="POST"
-          >
-          <input type="email" className="form-input" placeholder="enter email" name="_replyto" />
-          <button type="submit" className="submit-btn">
-            subscribe
-          </button>
-        </form>
+        <div style={{ overflow: 'hidden', background: 'none !important', maxWidth: '100%' }}>
+          <iframe
+            title="map"
+            width="600"
+            height="500"
+            id="gmap_canvas"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1886.7527854502293!2d72.82688963809264!3d18.95326688207639!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7cf648d0ee98d%3A0xeb29e8e92cb17eb6!2sMahaveer%20Flowers!5e0!3m2!1sen!2sus!4v1644412058803!5m2!1sen!2sus"
+            frameborder="0"
+            scrolling="no"
+            marginheight="0"
+            marginwidth="0">
+          </iframe>
+        </div>
       </div>
     </div>
   </Wrapper>
@@ -69,6 +79,15 @@ const Wrapper = styled.section`
   }
   .submit-btn:hover {
     color: var(--clr-white);
+  }
+  .service {
+    background: var(--clr-grey-9);
+    text-align: center;
+    padding: 2.5rem 2rem;
+    border-radius: var(--radius);
+    p {
+      color: var(--clr-primary-2);
+    }
   }
   @media (min-width: 992px) {
     .content {
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,20 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useFilterContext } from '../context/filter_context';
 import GridView from './GridView';
 import ListView from './ListView';
 
 const ProductList = () => {
   const { filtered_products: products, grid_view } = useFilterContext();
+  const [currentPage, setCurrentPage] = useState(1)
 
   if (products.length < 1) {
     return <h5 style={{ textTransform: 'none' }}>Sorry, no products matched your search..</h5>;
   }
 
-  if (grid_view === false) {
-    return <ListView products={products} />;
+  const itemsPerPage = 2
+
+  const handleClick = (event) => setCurrentPage(Number(event.target.id))
+
+  const handlePrevClick = () => setCurrentPage(currentPage - 1)
+  const handleNextClick = () => setCurrentPage(currentPage + 1)
+  const indexOfLastItem = currentPage * itemsPerPage
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage
+  const currentItems = products.slice(indexOfFirstItem, indexOfLastItem)
+
+  // Logic for displaying page numbers
+  const pageNumbers = []
+  for (let i = 1; i <= Math.ceil(products.length / itemsPerPage); i++) {
+    pageNumbers.push(i)
   }
 
-  return <GridView products={products}>product list</GridView>;
+  const renderPageNumbers = pageNumbers.map(number => {
+    const style = number === currentPage ? 'page-item active' : 'page-item'
+    return (
+      <li className={style}
+        key={number}
+        id={number}
+        onClick={handleClick}>
+        <button className='page-link button-link' id={number}>{number}</button></li>
+    )
+  })
+
+  let container = grid_view === false ? <ListView products={currentItems} /> : <GridView products={currentItems}>product list</GridView>
+  return <>
+    {container}
+    {products.length > itemsPerPage &&
+      <div className='row mt-5' style={{marginTop: '2rem'}}>
+        <div className='col-md-12 text-center'>
+          <nav aria-label='Page navigation' className='text-center'>
+            <ul className='pagination'>
+              {currentPage !== 1 && <li className='page-item' key='prev' onClick={handlePrevClick}><button className='page-link button-link'>&lt;</button></li>}
+              {renderPageNumbers}
+              {currentPage !== Math.ceil(products.length / itemsPerPage) && <li className='page-item' key='next' onClick={handleNextClick}><button className='page-link button-link' href='#'>&gt;</button></li>}
+            </ul>
+          </nav>
+        </div>
+      </div>
+    }
+  </>
 };
 
 export default ProductList;
