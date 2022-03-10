897d33e23a260842cc6342981c44c855280671a9
Rahul Nahar <rahul.nahar@cimpress.com>
2022-03-10 22:24:45 +0530

    pwa changes

---
+++ public/serviceWorker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = "version-1";
+const urlsToCache = ['index.html', 'offline.html', 'frame.png'];
+
+const self = this;
+
+// Install SW
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+
+                return cache.addAll(urlsToCache);
+            })
+    )
+});
+
+// Listen for requests
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request)
+                    .catch((err) => {
+                        if (event.request.url.includes('frame.png'))
+                            return caches.match('frame.png')
+                        else
+                            return caches.match('offline.html')
+                    })
+            })
+    )
+});
+
+// Activate the SW
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if (!cacheWhitelist.includes(cacheName)) {
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+
+    )
+});
