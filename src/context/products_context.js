fe97c3eba5a5677f041cbde97f2ca00f78bee1c1
Rahul Nahar <rahul.nahar@cimpress.com>
2023-01-18 12:00:35 +0530

    changes to merge sriMahaveer to mahaveer

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -1,6 +1,9 @@
 import React, { useContext, useEffect, useReducer } from 'react'
 import reducer from '../reducers/products_reducer'
+import axios from 'axios'
 import data from '../products.json'
+import _ from 'lodash'
+
 import {
   SIDEBAR_OPEN,
   SIDEBAR_CLOSE,
@@ -52,7 +55,22 @@ export const ProductsProvider = ({ children }) => {
     dispatch({ type: GET_PRODUCTS_BEGIN })
     
     try {
-      const products = data.products;
+      const { data: data1 } = await axios.get(
+        `https://api.srimahaveerflowers.in/api/products`
+      )
+
+      const products = [
+        ...data.products,
+        ...(_.map(data1, item => ({
+          categoryId: item.category,
+          id: item.product_id,
+          name: item.name,
+          images: item.image,
+          image: item.image[0].url
+        })
+        ))
+      ]
+
       dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products })
     } catch (error) {
       dispatch({ type: GET_PRODUCTS_ERROR });
@@ -63,7 +81,11 @@ export const ProductsProvider = ({ children }) => {
     dispatch({ type: GET_CATALOGS_BEGIN })
     
     try {
-      const categories = data.category;
+      const { data: data1 } = await axios.get(
+        `https://api.srimahaveerflowers.in/api/category`
+      )
+
+      const categories = [...data.category, ...(_.map(data1, item => ({ id: item.category_id, name: item.name, image: item.image })))]
       dispatch({ type: GET_CATALOGS_SUCCESS, payload: categories })
     } catch (error) {
       dispatch({ type: GET_CATALOGS_ERROR });
@@ -74,7 +96,19 @@ export const ProductsProvider = ({ children }) => {
     dispatch({ type: GET_SINGLE_PRODUCT_BEGIN });
 
     try {
-      const singleProduct = data.products.find(i => i.id === id.toString());
+      let singleProduct = state.products.find(i => i.id === id.toString());
+
+      if (!singleProduct) {
+        const { data: data1 } = await axios.get(`https://api.srimahaveerflowers.in/api/products/${id}`)
+        singleProduct = {
+          categoryId: data1.category,
+          id: data1.product_id,
+          name: data1.name,
+          images: data1.image,
+          image: data1.image[0].url
+        }
+      }
+
       await fetchRelatedProducts(singleProduct.categoryId, singleProduct.id);
       dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct });
 
@@ -87,7 +121,7 @@ export const ProductsProvider = ({ children }) => {
     dispatch({ type: GET_RELATED_PRODUCT_BEGIN });
 
     try {
-      const related_products = data.products.filter(i => i.categoryId === categoryId && i.id !== id);
+      const related_products = state.products.filter(i => i.categoryId === categoryId && i.id !== id);
       dispatch({ type: GET_RELATED_PRODUCT_SUCCESS, payload: related_products });
 
     } catch (error) {
