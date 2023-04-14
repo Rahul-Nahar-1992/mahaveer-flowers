0e97fbcd5311badf3810f6a4bfcbb59ccd020004
Rahul Nahar <rahul.nahar@cimpress.com>
2023-04-14 20:53:13 +0530

    minor

--- src/pages/OurWorkPage.js
+++ src/pages/OurWorkPage.js
@@ -7,11 +7,21 @@ const OurWorkPage = () => {
   return <main>
     <PageHero title="our work" />
     <Wrapper className="page section section-center">
-      <article>
+      <article class="ourWork">
         <div className="title">
           <h1>our work</h1>
           <div className="underline"></div>
         </div>
+        <p>
+          Welcome to our portfolio, where we display some of our greatest work! Mahaveer Flowers takes pleasure in crafting magnificent and one-of-a-kind flower arrangements for a wide range of events and occasions. 
+          Our team of talented specialists can convert any venue into a magnificent floral paradise, from weddings and business events to religious rituals and social gatherings.
+        </p>
+        <p>
+          Browse our portfolio for examples of our most recent work and gain ideas for your next event. Our floral arrangements are made with only the greatest quality artificial flowers, assuring their beauty and durability for a long time.
+        </p>
+        <p>
+          We think that every event deserves to be unforgettable, and we are dedicated to making your vision a reality. Contact us today to learn more about our services and how we can assist you in making your next event memorable.
+        </p>
         <ul className='videos'>
           {
             videoLink.map((item, i) => (
@@ -33,6 +43,14 @@ const Wrapper = styled.section`
     padding-top: 3rem;
       display: grid;
       gap: 2rem 1.5rem;
+  }
+  .ourWork p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 1rem;
+    color: var(--clr-grey-5);
+    font-size:1.25rem;
   }
   iframe {
     height: 20rem;
