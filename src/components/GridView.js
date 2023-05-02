4bafc09e398f770455d2587048cce146c99b7698
Rahul Nahar <rahul.nahar@cimpress.com>
2023-05-02 18:43:52 +0530

    ads

--- src/components/GridView.js
+++ src/components/GridView.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import Product from './Product';
-import { Ads } from './Ads';
 
 const GridView = ({ products }) => {
   return (
     <Wrapper>
-      <Ads />
       <div className='products-container'>
         {products.map((product) => {
           return <Product key={product.id} {...product} />;
