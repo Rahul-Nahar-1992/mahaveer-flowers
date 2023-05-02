44b2277861ac02a6764d880c1e454b391075138b
Rahul Nahar <rahul.nahar@cimpress.com>
2023-05-02 18:49:39 +0530

    Changes

--- src/components/Ads.js
+++ src/components/Ads.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import AdSense from 'react-adsense'
+import React, { useEffect } from 'react';
 
 export const Ads = () => {
+  useEffect(() => {
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+  }, [])
+
   return (
-    <AdSense.Google
-      client='ca-pub-5785569447311216'
-      slot='2509254784'
-      style={{ display: 'inline-block', width: '728px', height: '90px' }}
-    />
+    <ins className="adsbygoogle"
+      style={{ display: 'block' }}
+      data-ad-client="ca-pub-5785569447311216"
+      data-ad-slot="2509254784"
+      data-ad-format="auto"
+      data-full-width-responsive="true"></ins>
   );
 }
