e8b2198a826e5e73b6ce1b3ff09a19e29e0b8adf
Rahul Nahar <rahul.nahar@cimpress.com>
2023-05-03 19:38:34 +0530

    analytics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { Suspense, lazy } from 'react';
+import React, { Suspense, lazy, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar, Sidebar, Footer, Loading } from './components';
 import ScrollToTop from './components/ScrollToTop';
 import OurWorkPage from './pages/OurWorkPage';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-185663515-1');
 
 const Home = lazy(() => import('./pages/HomePage'));
 const SingleProduct = lazy(() => import('./pages/SingleProductPage'));
@@ -13,7 +15,13 @@ const Products = lazy(() => import('./pages/ProductsPage'));
 const PrivacyPolicyPage = lazy(() => import('./pages/PrivacyPolicyPage'));
 const DisclaimerPage = lazy(() => import('./pages/DisclaimerPage'));
 
+
 function App() {
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [])
+
   return (
     <Router>
 
