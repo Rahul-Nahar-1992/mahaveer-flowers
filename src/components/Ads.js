2ce8d60c1f31025dd5b3d85e82855edd0baa4024
Rahul Nahar <rahul.nahar@cimpress.com>
2023-05-04 21:41:23 +0530

    Minor changes

--- src/components/Ads.js
+++ src/components/Ads.js
@@ -6,14 +6,9 @@ export const Ads = () => {
   }, [])
 
   return (
-    <>
-      <ins className="adsbygoogle"
-        style={{ display: 'block' }}
-        data-ad-client="ca-pub-5785569447311216"
-        data-ad-slot="2831159739"
-        // data-ad-format="auto"
-        data-adtest="on"
-        data-full-width-responsive="true"></ins>
-    </>
+    <ins className="adsbygoogle"
+      style={{ display: 'block' }}
+      data-ad-client="ca-pub-5785569447311216"
+      data-ad-slot="2831159739"></ins>
   );
 }
